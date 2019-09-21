a3b15d2504b724ede3c2490e885851ba8055cd68
Oleg <gogoleg2008@gmail.com>
2019-09-22 02:22:29 +0300

    commit #9 - 22.09.2019

---
+++ test/com/epam/project/task01/utils/ArrayUtilTest.java
@@ -0,0 +1,40 @@
+package com.epam.project.task01.utils;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class ArrayUtilTest {
+
+    @Test
+    public void testFindMin() {
+    }
+
+    @Test
+    public void testFindMax() {
+    }
+
+    @Test
+    public void testSum() {
+    }
+
+    @Test
+    public void testAverage() {
+    }
+
+    @Test
+    public void testReplace() {
+    }
+
+    @Test
+    public void testSortBubble() {
+    }
+
+    @Test
+    public void testSortShell() {
+    }
+
+    @Test
+    public void testSortMerge() {
+    }
+}

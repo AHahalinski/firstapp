c66b264d255ad35522ad81f76b3e4e456c05a5e6
Oleg <gogoleg2008@gmail.com>
2019-09-25 18:48:29 +0300

    correct word "null"

--- src/com/epam/project/task01/exception/ArrayIsNullHandlerException.java
+++ src/com/epam/project/task01/exception/ArrayIsNullHandlerException.java
@@ -1,7 +1,7 @@
 package com.epam.project.task01.exception;
 
 public class ArrayIsNullHandlerException extends RuntimeException {
-    private static final String MESSAGE = "Object 'array' is nul";
+    private static final String MESSAGE = "Object 'array' is null";
 
     public ArrayIsNullHandlerException() {
         super(MESSAGE);
